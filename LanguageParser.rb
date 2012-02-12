64f2ac79b7b77d0b0278770bc200dcae32b1ec6d
Carol Glenn <cglenn@canvas.is>
2012-02-12 17:50:20 -0500

    Separate out particles.

--- LanguageParser.rb
+++ LanguageParser.rb
@@ -1,12 +1,25 @@
 file = File.open("samplesong.txt", "r")
 
 words = []
+particles = []
+verbs = []
+adjectives = []
+unknown = []
 
 file.each_line do |row|
+  # manually pick out punctuation to avoid pulling international characters
   row = row.gsub(/\.|\!|\@|\#|\$|\%|\^|\&|\*|\(|\)|\"|\?|\:|\,/, "").downcase.split(" ")
   words += row
 end
+
 words = words.uniq
 
-puts words
-puts words.count
+words.each do |word|
+  if word.length < 4
+    particles << word
+  end
+end
+
+puts particles
+#puts words
+#puts words.count
