bcecfb3993972c3a3f6e33ab550e8326b73c5078
Carol Glenn <cglenn@canvas.is>
2012-02-12 17:41:52 -0500

    Separate all words into an array; remove duplicates.

--- LanguageParser.rb
+++ LanguageParser.rb
@@ -1,5 +1,12 @@
-file = File.open("fileTBD", "r")
+file = File.open("samplesong.txt", "r")
+
+words = []
 
 file.each_line do |row|
-  
+  row = row.gsub(/\.|\!|\@|\#|\$|\%|\^|\&|\*|\(|\)|\"|\?|\:|\,/, "").downcase.split(" ")
+  words += row
 end
+words = words.uniq
+
+puts words
+puts words.count
