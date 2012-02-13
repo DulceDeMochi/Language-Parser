76bb76e0f6a72e49969b3a6a0d91328af27bd201
Carol Glenn <cglenn@canvas.is>
2012-02-12 21:37:25 -0500

    Temporarily hardcoded file to quicken dev time. Added more logic for looking up definitions.

--- LanguageParser.rb
+++ LanguageParser.rb
@@ -2,11 +2,11 @@
 require 'rubygems'
 require 'watir-webdriver'
 
-# Collect foreign words from a file for later translation
-puts "Enter file path:"
-path = gets
-file = File.open("#{path.chomp}", "r")
-
+## Collect foreign words from a file for later translation
+#puts "Enter file path:"
+#path = gets
+#file = File.open("#{path.chomp}", "r")
+file = File.open("samplesong.txt", "r")
 
 # Manually pick out punctuation to avoid pulling international characters
 words = []
@@ -25,3 +25,12 @@ end
 
 words -= particles
 
+# Open a browser to SpanishDict to look up definitions
+browser = Watir::Browser.new :ff
+url = "http://www.spanishdict.com/"
+
+browser.goto url
+
+#puts particles
+#puts '*' * 20
+#puts words
