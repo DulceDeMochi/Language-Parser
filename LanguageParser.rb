c362bfe8a08b103592c09ed5a21b1c027bdffc77
Carol Glenn <cglenn@canvas.is>
2012-02-12 22:07:28 -0500

    Code to submit form and begin pulling out parts of speech and definitions.

--- LanguageParser.rb
+++ LanguageParser.rb
@@ -28,8 +28,27 @@ words -= particles
 # Open a browser to SpanishDict to look up definitions
 browser = Watir::Browser.new :ff
 url = "http://www.spanishdict.com/"
-
 browser.goto url
+
+# Submit the word to the translation form
+browser.text_field(:id, "translation-textarea").set words[0]
+browser.form(:id, "translate-form").submit
+
+# Loop over spans and pull part of speech and first 2 possible definitions
+parts_of_speech = []
+definitions = []
+definition_count = 0
+
+browser.spans.each do |span|
+  if span.class_name == "part_of_speech"
+    parts_of_speech << span.text
+  elsif span.class_name == "def" && definition_count < 3
+    definitions << span.text
+    definition_count += 1
+  end
+end
+
+puts parts_of_speech
 
 #puts particles
 #puts '*' * 20
