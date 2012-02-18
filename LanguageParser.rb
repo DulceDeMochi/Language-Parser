9ffe653bdeeda608db80fda65bcd2847846a2fd4
Carol Glenn <carolglenn@Carols-MacBook-Pro.local>
2012-02-18 14:35:07 -0500

    Added awesome print and reduced wait time between lookups.

--- LanguageParser.rb
+++ LanguageParser.rb
@@ -1,11 +1,12 @@
 require 'rubygems'
 require 'watir-webdriver'
+require 'ap'
 
 ## Collect foreign words from a file for later translation
 #puts "Enter file path:"
 #path = gets
 #file = File.open("#{path.chomp}", "r")
-file = File.open("samplesong.txt", "r")
+file = File.open("samplesong2.txt", "r")
 
 # Manually pick out punctuation to avoid pulling international characters
 words = []
@@ -33,7 +34,7 @@ browser.goto url
 defined_words = Hash.new
 
 word_count = 0
-until word_count == 5
+until word_count == 10
   # Submit the word to the translation form
   browser.text_field(:id, "translation-textarea").set words[word_count]
   browser.form(:id, "translate-form").submit
@@ -56,10 +57,10 @@ until word_count == 5
 
   defined_words[words[word_count]] = definitions
   word_count += 1
-  sleep 30
+  sleep 15
 end
 
-puts defined_words
+ap defined_words
 
 #puts particles
 #puts '*' * 20
